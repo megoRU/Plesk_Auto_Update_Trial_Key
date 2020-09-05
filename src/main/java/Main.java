c58f63e732f3eb75858618fbed0d14f6a638194f
megoRU <savin.yura2014@gmail.com>
2020-09-05 22:59:42 +0300

    add for(; ;) add remove trash add mysql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
-import com.jcraft.jsch.JSchException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 
@@ -14,26 +13,44 @@ public class Main {
   private static final String PASS = "";
   private static final HashMap<Integer, String> keys = new HashMap<Integer, String>();
 
-  public static void main(String[] args) throws JSchException, SQLException {
-    //bin/rm -rf /etc/sw/keys/keys/key*
-    //usr/sbin/plesk bin license -i ${key}
 
-    String sql = "SELECT id, text FROM Plesk " + "ORDER BY id DESC";
-    Connection conn = DriverManager.getConnection(CONN, USER, PASS);
-    Statement statement = conn.createStatement();
-    ResultSet rs = statement.executeQuery(sql);
+  public static void main(String[] args) {
+    try {
+      for (; ; ) {
+        String query = "SELECT id, text FROM Plesk WHERE id = 0";
+        String delete = "DELETE FROM Plesk WHERE id = ?";
+        String update = "UPDATE Plesk SET id = id - 1 WHERE id >= ?";
+        // String sql = "SELECT id, text FROM Plesk " + "ORDER BY id DESC";
+        Connection conn = DriverManager.getConnection(CONN, USER, PASS);
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        while (rs.next()) {
+          String id = rs.getString("id");
+          String text = rs.getString("text");
+          keys.put(Integer.parseInt(id), text);
+          // System.out.println(id + ". " + text + "\n");
+        }
 
+        System.out.println(keys.get(0));
 
-    while (rs.next()) {
-      String id = rs.getString("id");
-      String text = rs.getString("text");
-      keys.put(Integer.parseInt(id), text);
-     // System.out.println(id + ". " + text + "\n");
+        System.out.println(RunCommandViaSsh.runCommand("pwd"));
+        System.out.println(RunCommandViaSsh.runCommand("bin/rm -rf /etc/sw/keys/keys/key*"));
+        System.out
+            .println(RunCommandViaSsh.runCommand("usr/sbin/plesk bin license -i " + keys.get(0)));
+
+        //remove from DB and HashMap
+        keys.remove(0);
+        PreparedStatement preparedStmt = conn.prepareStatement(delete);
+        PreparedStatement preparedStmt2 = conn.prepareStatement(update);
+        preparedStmt.setInt(1, 0);
+        preparedStmt2.setInt(1, 0);
+        preparedStmt.executeUpdate();
+        preparedStmt2.executeUpdate();
+        //sleep 11,5 days
+        Thread.sleep(999999999);
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
-    //System.out.println(keys.get(0));
-
-//    System.out.println(RunCommandViaSsh.runCommand("pwd"));
-//    System.out.println(RunCommandViaSsh.runCommand("bin/rm -rf /etc/sw/keys/keys/key*"));
-//    System.out.println(RunCommandViaSsh.runCommand("usr/sbin/plesk bin license -i " + keys.get(0).toString()));
   }
 }
